78feebb4e1c5d2430799dfe99b08a560f44a26df
TomaszLaboj <tomasz.laboj@gmail.com>
2023-07-08 17:53:47 +0100

    run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,56 @@
 import "./myStyles.css";
-import names from "./data/babyNamesData.json"
+import names from "./data/babyNamesData.json";
 import { useState } from "react";
 
 function App(): JSX.Element {
-
   interface BabyNames {
-    id: number,
-    name: string,
-    sex: string,
+    id: number;
+    name: string;
+    sex: string;
   }
 
-  const babyNames:BabyNames[] = [];
+  const babyNames: BabyNames[] = [];
 
-    for(const element of names){
-        babyNames.push(element)
-    }
-  const sortedNames:BabyNames[]= babyNames.sort((a,b)=>(a.name>b.name)?1:-1)
-  const [name,setName] = useState('')
+  for (const element of names) {
+    babyNames.push(element);
+  }
+  const sortedNames: BabyNames[] = babyNames.sort((a, b) =>
+    a.name > b.name ? 1 : -1
+  );
+  const [name, setName] = useState("");
 
-  const handleChoseName = (clickedName:string) => {
+  const handleChoseName = (clickedName: string) => {
     setName(clickedName);
-
-  }
+  };
 
   const isMale = (checkSex: string) => {
-   return checkSex == 'm' ? true : false;
-  }
-
-
+    return checkSex == "m" ? true : false;
+  };
 
   return (
-      <body>
-        <div className="main">
-            <>
-              Chosen names:{name}
-            </>
+    <body>
+      <div className="main">
+        <>Chosen names:{name}</>
+      </div>
 
-        </div>
+      <br></br>
 
-        <br></br>
-         
-        <div className="main">
-      All names:{
-            babyNames.map(el => {
-              return(
-                
-
-                  <button
-                  className = {isMale(el.sex) ? "blue":"pink"}
-                  onClick={() => handleChoseName(el.name)}
-                  key={el.id}>{el.name}
-                  </button>
-                
-              )
-            })
-        }
-      
-      
-
-        </div>
-      </body>
-
-  )
-
-
+      <div className="main">
+        All names:
+        {babyNames.map((el) => {
+          return (
+            <button
+              className={isMale(el.sex) ? "blue" : "pink"}
+              onClick={() => handleChoseName(el.name)}
+              key={el.id}
+            >
+              {el.name}
+            </button>
+          );
+        })}
+      </div>
+    </body>
+  );
 }
 
 export default App;
