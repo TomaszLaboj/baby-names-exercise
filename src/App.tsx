8af502c997950449d0fbc590bd5c88e842895603
TomaszLaboj <tomasz.laboj@gmail.com>
2023-07-08 17:51:20 +0100

    buttons displaying, names sorted, button colours based on sex property, single name as current

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,69 @@
-
+import "./myStyles.css";
+import names from "./data/babyNamesData.json"
+import { useState } from "react";
 
 function App(): JSX.Element {
-  return <h1>Hello world</h1>;
+
+  interface BabyNames {
+    id: number,
+    name: string,
+    sex: string,
+  }
+
+  const babyNames:BabyNames[] = [];
+
+    for(const element of names){
+        babyNames.push(element)
+    }
+  const sortedNames:BabyNames[]= babyNames.sort((a,b)=>(a.name>b.name)?1:-1)
+  const [name,setName] = useState('')
+
+  const handleChoseName = (clickedName:string) => {
+    setName(clickedName);
+
+  }
+
+  const isMale = (checkSex: string) => {
+   return checkSex == 'm' ? true : false;
+  }
+
+
+
+  return (
+      <body>
+        <div className="main">
+            <>
+              Chosen names:{name}
+            </>
+
+        </div>
+
+        <br></br>
+         
+        <div className="main">
+      All names:{
+            babyNames.map(el => {
+              return(
+                
+
+                  <button
+                  className = {isMale(el.sex) ? "blue":"pink"}
+                  onClick={() => handleChoseName(el.name)}
+                  key={el.id}>{el.name}
+                  </button>
+                
+              )
+            })
+        }
+      
+      
+
+        </div>
+      </body>
+
+  )
+
+
 }
 
 export default App;
