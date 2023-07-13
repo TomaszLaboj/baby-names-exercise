5df89778437579950ef29ff9ebb9727dee588171
TomaszLaboj <tomasz.laboj@gmail.com>
2023-07-13 08:08:21 +0100

    moving buttons between areas works, filter works, no gender selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,74 @@
 import "./myStyles.css";
 import names from "./data/babyNamesData.json";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BabyNames from "./utils/BabyNames.js"
 
 
 function App():JSX.Element{
  
  const babyNames: BabyNames[] = names.sort((a,b)=> a.name>b.name ? 1: -1);
+
  const [chosenNames,setChosenNames] = useState<BabyNames[]>([])
  const [allNames, setAllNames] = useState<BabyNames[]>(babyNames)
+ const [text , setText] = useState('') // state for search bar
 
- const handleChooseName = (clickedName: BabyNames) => {
+
+ useEffect(() => {  
+   setAllNames(babyNames.filter((names)=>names.name.toLowerCase().includes(text.toLowerCase()))); 
+},[text]);
+const handleChooseName = (clickedName: BabyNames) => {
   setChosenNames([...chosenNames,clickedName]);
   setAllNames(allNames.filter((obj)=> obj!== clickedName))
 } 
-
- const handleMoveBack = (clickedName: BabyNames) => {
+const handleMoveBack = (clickedName: BabyNames) => {
   setAllNames([...allNames,clickedName].sort((a,b)=> a.name>b.name ? 1: -1));
-  
   setChosenNames(chosenNames.filter((obj)=> obj !== clickedName))
- }
- function sortingNames(arrayNames:BabyNames[]):BabyNames[]{
+}
 
-  return arrayNames.sort((a,b)=> a.name>b.name?1:-1);
- }
+
+ const isMale = (checkSex: string) => {
+  return checkSex === "m" ? true : false;
+};
+
   return (
-    <div className="main">
-      <p className="main">
-      Chosen names:{chosenNames.map((chosenName) => {
+    <body>
+
+      <div className="main">
+        <> 
+        Chosen names:{chosenNames.map((chosenName) => {
+          return (
+            <button
+            className={isMale(chosenName.sex) ? "blue" : "pink"}
+            onClick={()=>handleMoveBack(chosenName)}
+            key={chosenName.id}
+            >{chosenName.name}
+
+            </button>
+          )
+        })}
+        </>
+      </div>
+        <br/>
+      <p className="main">  Filter: <input value={text} onChange={(event) => {
+          setText(event.target.value);
+          
+        }}/></p>
+      <br></br> 
+      <div className="main">
+      All names:
+      {allNames.map((babyName) => {
         return (
           <button
-          onClick={()=>handleMoveBack(chosenName)}
-          key={chosenName.id}
-          >{chosenName.name}
-
+          className={isMale(babyName.sex) ? "blue" : "pink"}
+            onClick={()=>handleChooseName(babyName)}
+            key={babyName.id}
+          >
+            {babyName.name}
           </button>
         )
       })}
-      </p>
-      <br/>
-    All names:
-    {allNames.map((babyName) => {
-      return (
-        <button
-          
-          onClick={()=>handleChooseName(babyName)}
-          key={babyName.id}
-        >
-          {babyName.name}
-        </button>
-      )
-    })}
       </div>
+    </body>
     )
   }
 export default App;
